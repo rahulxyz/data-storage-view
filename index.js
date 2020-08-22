738c4c3d3eac54b1d22c7dc59151a389aa4fd0af
rahulxyz <rahulguptaxyz15@gmail.com>
2020-08-23 00:14:38 +0530

    mobile design added

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 const slider = document.getElementById("slider");
 const sliderValue = document.getElementById("tooltip");
+const storageOccupied = document.getElementById("storage-occupied");
 
-slider.oninput = function(){
-   sliderValue.innerHTML = this.value+ "<span>&nbsp;&nbsp;GB LEFT</span>";
+slider.oninput = function () {
+    sliderValue.innerHTML = 1000 - this.value + "<span>&nbsp;&nbsp;GB LEFT</span>";
+    storageOccupied.innerHTML = this.value + " GB";
 };
 
-['mousemove','touchmove'].forEach(evt=>
-    slider.addEventListener(evt, function(){
+['mousemove', 'touchmove'].forEach(evt =>
+    slider.addEventListener(evt, function () {
         setSliderStyle();
     })
 );
 
-slider.addEventListener('mouseover',()=>{
+slider.addEventListener('mouseover', () => {
     sliderValue.style.visibility = 'visible';
 })
 
-slider.addEventListener('mouseout',()=>{
+slider.addEventListener('mouseout', () => {
     sliderValue.style.visibility = 'hidden';
 })
 
-setSliderStyle = () =>{
+setSliderStyle = () => {
     let x = slider.value;
     x /= 10;
 
     let transparent = `linear-gradient(to right, transparent ${x}%, var(--very-dark-blue) ${x}%)`;
     let lin = `linear-gradient(to right, var(--light-pink) , var(--dark-pink))`;
-    slider.style.background = transparent+","+lin;
+    slider.style.background = transparent + "," + lin;
 }
 
 // Run once at start
